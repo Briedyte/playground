0e3a4dddfbe19ff71e6a8bf9727535026eefead9
Emilija Briedyte <em.bredyte@gmail.com>
2023-04-02 19:27:43 +0300

    Changed padding of Paragraph component

--- src/components/BalloonGame/Components/Paragraph.tsx
+++ src/components/BalloonGame/Components/Paragraph.tsx
@@ -1,10 +1,11 @@
-import { FontSize, MediaQuery } from 'config/style';
+import { FontSize, MediaQuery, Spacing } from 'config/style';
 import styled from 'styled-components';
 
 const ParagraphStyled = styled.p`
   text-align: center;
   font-size: ${FontSize[40]};
   pointer-events: none;
+  padding: ${Spacing[16]};
 
   ${MediaQuery.s} {
     font-size: 30px;
